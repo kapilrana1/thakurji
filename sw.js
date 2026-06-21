6eded78c171b6ab172357a4e5c2cf56b1f732e09
kapilrana1 <rana.kapil9@gmail.com>
2026-06-21 16:43:05 +0530

    Fix ticker CSS, add popup alert feature, bump SW cache ta-v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ta-v3';
+const CACHE = 'ta-v4';
 const ASSETS = ['/', '/index.html', '/favicon.svg',
   'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&family=Playfair+Display:wght@500;700&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
